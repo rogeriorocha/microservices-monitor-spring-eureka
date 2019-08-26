04716e010a197558f4ea83f2b0d4deb16bebb8a7
Rogerio Rocha <rogeriosilvarocha@gmail.com>
2019-08-26 10:32:46 -0300

    changes

---
+++ organization-service/src/main/java/pl/piomin/services/organization/client/intercept/FeignRequestInterceptor.java
@@ -0,0 +1,14 @@
+package pl.piomin.services.organization.client.intercept;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+public class FeignRequestInterceptor implements RequestInterceptor{
+
+	@Override
+	public void apply(RequestTemplate template) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
